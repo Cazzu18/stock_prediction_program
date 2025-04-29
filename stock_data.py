2625ec8aead38b04d4427e6fbaa7784fbad478f0
Edmund Howell III Longsworth <99919831+Cazzu18@users.noreply.github.com>
2025-04-28 22:20:33 -0500

    Delete stock_data.py

--- stock_data.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-def fetch_yahoo_finance_stock_data(ticker, start_date, end_date):
-    stock = yf.download(ticker, start=start_date, end=end_date)
-    if 'Close' not in stock.columns or stock['Close'].empty:
-        return pd.Series(dtype=float)
-    return stock['Close']
